02798e7603ff69112680469cd04e14cf37706c8c
Ismael Garcia <igarcia99@pusd.pcoe.k12.ca.us>
2019-03-29 15:11:47 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let can;
+
+function setup() {
+can=createCanvas(500,400);
+can.position(100,100);
+can.class("bdd");
+
+}
+
+function draw() {
+//clear();
+background(250,0,0);
+rect(40,40,20,50);
+fill(0,250,0);
+ellipse(80,80,100,30);
+}
